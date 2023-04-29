0e8c55e87880f9b13ebcb12140cb058027ab1a66
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:42:25 +0300

    circle çizebildim

---
+++ OpenClosedprinciple/Good/Circle.cs
@@ -0,0 +1,17 @@
+﻿using OpenClosedprinciple.Good;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple.Good
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
