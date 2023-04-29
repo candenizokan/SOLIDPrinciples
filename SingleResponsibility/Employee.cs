25009f331c955b5da76906cd0d9c4a566859825a
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:12:16 +0300

    CreateReport u employe içinden çıkardım. herkes herkesle çalışsın diye parçalamayı tercih ediyorum

--- SingleResponsibility/Employee.cs
+++ SingleResponsibility/Employee.cs
@@ -17,6 +17,17 @@ namespace SingleResponsibility
             //işlemlerin yapıldığı düşünüldüğünde
         }
 
+        //public void CreateReport(Employee employee)
+        //{
+        //    // x formatta çıktı vererek çalışan bilgilerini rapor eden metot.
+        //    // ama x formatta çıktı (pdf, xml, json, excel,...) olabilir ve değişmek istendiğinde yada rapor vermenin başka sınıflar tarafında da kullanılması gerektiğinde burada değişkenler yapmak ve sorumluluk aşılmış olacak ve bu metodun değişimi tek bir nedene dayalı olmayacak. Bu yüzden aslında bu metodun başka bir sınıfa yerleştirilmesi ve employee nin bu sınıfla çalışması daha mantıklı ki metod diğer sınıflara da hizmet edebilsin.
+
+        //    // rapor vermek daha genel. ayrıca bugün xml verebilirim ama yarın pdf jpg isteyebilirler.
+        //}
+    }
+
+    public class Kontrol
+    {
         public void CreateReport(Employee employee)
         {
             // x formatta çıktı vererek çalışan bilgilerini rapor eden metot.
