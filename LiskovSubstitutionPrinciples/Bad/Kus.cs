5de0a6261c78727d07850055c11ee9633bdc1d9b
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 13:22:16 +0300

    add kus

---
+++ LiskovSubstitutionPrinciples/Bad/Kus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciples.Bad
+{
+    public abstract class Kus
+    {
+        public abstract string Uc { get; }
+        public abstract string Yuru { get; }
+        public abstract string Beslen { get; }
+    }
+}
