b17e1cab0f73cd417b4f5ce4f84bebe32e29943f
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 13:41:35 +0300

    Solid I

---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregationPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interfacelerin ayrılması prensibidir. Sadece yapabiliyoruz diye tüm prensipler tek bir interface içine yazılmamalı gerekiyorsa parçalaanmalıdır. hizmet ve yaklaşım birliği olan yada bir kaç metod imzası tabiki tek interface olabilir. adet vs sınırlaması yoktur nihayetinde yazılacak metod imzası saıyısı aynıdır.
+        }
+    }
+}
