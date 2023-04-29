a09f5713b81afa61ab57abb8e76e01746910f7e7
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:10:42 +0300

    employee de rapor var bu buraya hizmet etmemesi gerekiyor

---
+++ SingleResponsibility/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TCno { get; set; }
+        public bool AddEmployee(Employee employee)
+        {
+            return true;
+            //işlemlerin yapıldığı düşünüldüğünde
+        }
+
+        public void CreateReport(Employee employee)
+        {
+            // x formatta çıktı vererek çalışan bilgilerini rapor eden metot.
+            // ama x formatta çıktı (pdf, xml, json, excel,...) olabilir ve değişmek istendiğinde yada rapor vermenin başka sınıflar tarafında da kullanılması gerektiğinde burada değişkenler yapmak ve sorumluluk aşılmış olacak ve bu metodun değişimi tek bir nedene dayalı olmayacak. Bu yüzden aslında bu metodun başka bir sınıfa yerleştirilmesi ve employee nin bu sınıfla çalışması daha mantıklı ki metod diğer sınıflara da hizmet edebilsin.
+
+            // rapor vermek daha genel. ayrıca bugün xml verebilirim ama yarın pdf jpg isteyebilirler.
+        }
+    }
+}
