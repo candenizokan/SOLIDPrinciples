8e7669ab644354eefaac2ff5d4e6f6b83d8552f9
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 13:16:04 +0300

    Solid L

---
+++ LiskovSubstitutionPrinciples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Liskov substitution principles => Matematikçi liskov tarafından oluşturulan prensip der ki: türeyen sınıflar (kalıtım alan), kalıtım aldıkları yeni geldikleri sınıfın içindeki tüm metod ve özelliklere sahip ve tam uygulayabilir olmalı yani türediği sınıfın yeine geçebilmelidir.
+
+            // aksi durum LSP'ye aykırıdır ve güvenip atasının yerine gönderdiğimiz bir sınıf ile sorun yaşarız. sınıfın kalıtım alması yada ilgili özellik/ yeteneğin ata sınıfta olması üzerine düşünmek gerekir.
+
+
+        }
+    }
+}
