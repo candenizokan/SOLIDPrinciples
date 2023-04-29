46250476648fee938245ecb627e8628ca016872f
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 13:28:32 +0300

    tavuk

---
+++ LiskovSubstitutionPrinciples/Bad/Tavuk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitutionPrinciples.Bad
+{
+    public class Tavuk : Kus
+    {
+        public override string Beslen()
+        {
+            return "beslendi";
+        }
+
+        public override string Uc()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Yuru()
+        {
+            return "yurudu";
+        }
+    }
+}
