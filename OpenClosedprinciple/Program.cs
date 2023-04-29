5180d96adc60bf03d14b94a0f3bcc99f580d8b02
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:19:32 +0300

    Open Closed Principle

---
+++ OpenClosedprinciple/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open Closed Principle => açık- kapalı prensip. Gelişime Açık : gelen yeni sınıflarla çalışabilir. adepte olabilir ama Değişime Kapalı: gelen yeni yapılar / sınıflarla çalışırken bunu mevcut kodlarımıza sürekli kod ekleyerek yapmadan sağlamalıyız.
+        }
+    }
+}
