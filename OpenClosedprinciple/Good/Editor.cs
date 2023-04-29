1052a48df574a07492646970fe94f24d24283177
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:38:20 +0300

    good shape

---
+++ OpenClosedprinciple/Good/Editor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedprinciple.Good
+{
+    public class Editor
+    {
+        public void DrawShape(Shape shape)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ OpenClosedprinciple/Good/Shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedprinciple.Good
+{
+    public abstract class Shape
+    {
+        public abstract void Draw();//abstract olduğu için ezilecek neden gövdesini oluşturayım böylee kalsın.
+        // abstract işaretli sınıfın içindeki abstract metod gittiği yerde zaten zileceğinde açıklaması gövdesi yoktur.
+    }
+}
