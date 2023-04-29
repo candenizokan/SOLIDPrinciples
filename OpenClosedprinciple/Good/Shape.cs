2550464a2d9d061fcba9b4e9c86d0c606a47074b
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:48:29 +0300

    open closed

--- OpenClosedprinciple/Good/Shape.cs
+++ OpenClosedprinciple/Good/Shape.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace OpenClosedprinciple.Good
 {
+    //burada yeni çizilmesi gereken her sınıf kendinin nasıl çizileceğini biliyor ve editör ihtiyaç duyduğu nesneyi soyut katmandan istiyor.
+    //bu yüzden shape abstract sınıfın kalıtım alan, yeni oluşturulan her sınıf editör tarafından editöre hiç bir kod eklemeden çizilebilir oluyor.
+    // gelişime (yeni şekillere açık ama) değişime(editör sınıf kodlarının değişime kapalı) olunmuş oluyor
     public abstract class Shape
     {
         public abstract void Draw();//abstract olduğu için ezilecek neden gövdesini oluşturayım böylee kalsın.
