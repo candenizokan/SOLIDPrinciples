4a2b33ec2887707deeb11cadbee6428b46b91803
Okan Candeniz <candenizokan@gmail.com>
2023-04-29 12:40:49 +0300

    editör artık ücgeni çizebilri

---
+++ OpenClosedprinciple/Good/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using OpenClosedprinciple.Good;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple.Good
+{
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            //kod blok
+        }
+    }
+}
