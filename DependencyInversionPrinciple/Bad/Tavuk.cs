3659e24845da7b992de1a49d396fcc47a03515c2
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 10:29:26 +0300

    add tavuk

---
+++ DependencyInversionPrinciple/Bad/Tavuk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple.Bad
+{
+    public class Tavuk
+    {
+        public void Pisir()
+        {
+
+        }
+    }
+}
