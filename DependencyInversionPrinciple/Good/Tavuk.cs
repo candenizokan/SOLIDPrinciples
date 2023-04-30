7ea6b1b84b83a7b05530393f5f39dccb680914f1
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 10:37:59 +0300

    good tavuk inherit

---
+++ DependencyInversionPrinciple/Good/Tavuk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple.Good
+{
+    public class Tavuk : IUrun
+    {
+        public void Pisir()
+        {
+            //kod blok
+        }
+    }
+}
