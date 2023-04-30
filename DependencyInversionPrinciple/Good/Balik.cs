1869909ff28fcee131360933a70b2915edee39af
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 10:36:56 +0300

    good balik-inherit

---
+++ DependencyInversionPrinciple/Good/Balik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple.Good
+{
+    public class Balik : IUrun
+    {
+        public void Pisir()
+        {
+            //kod blok
+        }
+    }
+}
