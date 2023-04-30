10a6eb5287f3e8b73add61fe6c11d425a85b3d5a
Okan Candeniz <candenizokan@gmail.com>
2023-04-30 10:28:15 +0300

    add rstoran

---
+++ DependencyInversionPrinciple/Bad/Restoran.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple.Bad
+{
+    public class Restoran
+    {
+        Balik balik = new Balik();
+
+        public void YemekHazirla()
+        {
+            balik.Pisir();
+        }
+    }
+}
